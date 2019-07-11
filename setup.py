60bbe6b589221d5c0cb98b2303ee2ace7513e843
Lu Xing <32466099+glorialulu@users.noreply.github.com>
2019-07-11 14:28:42 -0500

    requirements updated

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'math', 'wntr','sys',
+requirements = ['numpy', 'wntr','sys',
                 'datetime', 'warnings', 'functools',
                 'matplotlib', 'networkx', 'logging']
 
