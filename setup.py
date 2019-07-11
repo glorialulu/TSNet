fd0e36ad9499c84fdf979a632dfe282759ade842
Lu Xing <32466099+glorialulu@users.noreply.github.com>
2019-07-11 15:11:13 -0500

    update docs

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy',  'pandas', 'scipy',
+requirements = ['numpy', 'pandas', 'scipy',
                 'matplotlib', 'networkx', 
                 'plotly', 'wntr',]
 
